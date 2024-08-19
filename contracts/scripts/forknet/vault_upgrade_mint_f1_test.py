3e73e15f7dac38decf761b41e3c4ac6dde5e95df
Trisome-Bedrock <trisome@bedrock-technology>
2024-08-19 15:49:08 +0800

    fix script

--- contracts/scripts/forknet/vault_upgrade_mint_f1_test.py
+++ contracts/scripts/forknet/vault_upgrade_mint_f1_test.py
@@ -71,20 +71,23 @@ def main(network="ethereum"):
 
     amt = 0.05 * 1e8
 
-    tx = fbtc_proxy.mintLockedFbtcRequest(amt, {'from': owner_zealy})
-    assert tx.status == 1
-    assert len(tx.events) > 0
-    assert 'MintLockedFbtcRequest' in tx.events
+    with brownie .reverts("USR001"):
+        tx = fbtc_proxy.mintLockedFbtcRequest(amt, {'from': owner_zealy})
 
-    receive_amount = tx.events['MintLockedFbtcRequest']['receivedAmount']
-    fee = tx.events['MintLockedFbtcRequest']['fee']
-    assert receive_amount > 0
-    assert locked_fbtc.balanceOf(vault) == receive_amount
-    assert fbtc.balanceOf(vault) == fbtc_bal_before - amt
-
-    print("Network: ", network)
-    print(" Burned FBTC amount: ", amt)
-    print(" Minted LockedFBTC amount: ", receive_amount)
-    print(" Fee: ", fee)
+#     tx = fbtc_proxy.mintLockedFbtcRequest(amt, {'from': owner_zealy})
+#     assert tx.status == 1
+#     assert len(tx.events) > 0
+#     assert 'MintLockedFbtcRequest' in tx.events
+#
+#     receive_amount = tx.events['MintLockedFbtcRequest']['receivedAmount']
+#     fee = tx.events['MintLockedFbtcRequest']['fee']
+#     assert receive_amount > 0
+#     assert locked_fbtc.balanceOf(vault) == receive_amount
+#     assert fbtc.balanceOf(vault) == fbtc_bal_before - amt
+#
+#     print("Network: ", network)
+#     print(" Burned FBTC amount: ", amt)
+#     print(" Minted LockedFBTC amount: ", receive_amount)
+#     print(" Fee: ", fee)
 
 
